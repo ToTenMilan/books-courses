8f2874ea230cb5f5312d75e9198f20c4ea6c99eb
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2018-01-13 13:21:54 +0100

    Server not running

--- shine/config/webpack/environment.js
+++ shine/config/webpack/environment.js
@@ -3,18 +3,5 @@ const typescript =  require('./loaders/typescript')
 
 environment.loaders.append('typescript', typescript)
 
-environment.loaders.set('html', {
-  test: /\.html$/,
-  use: [{
-    loader: 'html-loader',
-    options: {
-      minimize: true,
-      removeAttributeQuotes: false,
-      caseSensitive: true,
-      customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
-      customAttrAssign: [ /\)?\]?=/ ]
-    }
-  }]
-})
 
 module.exports = environment
