4b99467411479f22fa78ad714614db287382b229
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2018-01-13 13:58:04 +0100

    Working html-loader, omitted chapter 7 - testing

--- shine/app/javascript/packs/customers.js
+++ shine/app/javascript/packs/customers.js
@@ -6,92 +6,61 @@
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/dcbang2 for more book information.
 ***/
-import "hello_angular/polyfills";
+import "polyfills";
 
-import { Component, NgModule    } from "@angular/core";
+import { NgModule               } from "@angular/core";
 import { BrowserModule          } from "@angular/platform-browser";
 import { FormsModule            } from "@angular/forms";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { Http, HttpModule } from "@angular/http";
+import { HttpModule              } from "@angular/http";
+import { CustomerSearchComponent } from "CustomerSearchComponent";
 
-var CustomerSearchComponent = Component({
-  selector: "shine-customer-search",
-  template: '\
-<header> \
-  <h1 class="h2">Customer Search</h1> \
-</header> \
-\
-<section class="search-form"> \
-  <form> \
-    <label for="keywords" class="sr-only">Keywords></label> \
-    <input type="text" id="keywords" name="keywords" \
-           placeholder="First Name, Last Name, or Email Address"\
-           class="form-control input-lg" \
-           bind-ngModel="keywords"\
-           on-ngModelChange="search($event)">\
-\
-  </form> \
-</section> \
-\
-<section class="search-results"> \
-  <header> \
-    <h1 class="h3">Results</h1> \
-  </header> \
-  <ol class="list-group"> \
-    <li *ngFor="let customer of customers" class="list-group-item clearfix"> \
-      <h3 class="pull-right"> \
-        <small class="text-uppercase">Joined</small> \
-        {{customer.created_at}}\
-      </h3> \
-      <h2 class="h3"> \
-        {{ customer.first_name }} {{ customer.last_name }}\
-        <small>{{ customer.user_name }}</small> \
-      </h2> \
-      <h4>{{ customer.email }}</h4> \
-    </li> \
-  </ol> \
-</section> \
-  '
-}).Class({
-  // constructor: function() {
-  //   this.customers = null;
-  //   this.keywords = "";
-  // },
-  constructor: [
-    Http, // this needs HttpModule imported by NgModule
-    function(http) {
-      this.customers = null;
-      this.http = http; // this needs Http in array above
-      this.keywords = '';
-    }
-  ],
-
-  search: function($event) {
-    var self = this;
-    // request
-    self.keywords = $event;
-    if (self.keywords.length < 3) {
-      return;
-    }
-
-    self.http.get( // this needs this.http in constructor
-      "/customers.json?keywords=" + self.keywords // RAILS ANGULAR Connection
-    ).subscribe(
-    // response
-      function(response) {
-        self.customers = response.json().customers
-      },
-      // response errors
-      function(response) {
-        window.alert(response)
-      }
-    );
+var RESULTS = [
+  {
+    first_name: "Pat",
+    last_name: "Smith",
+    username: "psmith",
+    email: "pat.smith@somewhere.net",
+    created_at: "2016-02-05",
   },
-});
+  {
+    first_name: "Patrick",
+    last_name: "Jones",
+    username: "pjpj",
+    email: "jones.p@business.net",
+    created_at: "2014-03-05",
+  },
+  {
+    first_name: "Patricia",
+    last_name: "Benjamin",
+    username: "pattyb",
+    email: "benjie@aol.info",
+    created_at: "2016-01-02",
+  },
+  {
+    first_name: "Patty",
+    last_name: "Patrickson",
+    username: "ppat",
+    email: "pppp@freemail.computer",
+    created_at: "2016-02-05",
+  },
+  {
+    first_name: "Jane",
+    last_name: "Patrick",
+    username: "janesays",
+    email: "janep@company.net",
+    created_at: "2013-01-05",
+  },
+];
+
+
 
 
 var CustomerAppModule = NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    HttpModule ],
   declarations: [ CustomerSearchComponent ],
   bootstrap:    [ CustomerSearchComponent ]
 })
@@ -100,11 +69,3 @@ var CustomerAppModule = NgModule({
 });
 
 platformBrowserDynamic().bootstrapModule(CustomerAppModule);
-
-
-// <!-- before was one way binding bindon-ngModel="keywords" -->
-// <span class="input-group-btn"> \
-//         <input type="submit" value="Find Customers"\
-//          class="btn btn-primary btn-lg"\
-//          on-click="search()">\
-//       </span> \
--- shine/config/webpack/environment.js
+++ shine/config/webpack/environment.js
@@ -2,4 +2,19 @@ const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.append('typescript', typescript)
+
+environment.loaders.set('html', {
+  test: /\.html$/,
+  use: [{
+    loader: 'html-loader',
+    options: {
+      minimize: true,
+      removeAttributeQuotes: false,
+      caseSensitive: true,
+      customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
+      customAttrAssign: [ /\)?\]?=/ ]
+    }
+  }]
+})
+
 module.exports = environment
