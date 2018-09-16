09b851596404033477744f947b10cde812fc3b5d
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-09-16 19:43:12 +0200

    Finish ch. 9

--- understanding-es6/9_intro_to_classes.js
+++ understanding-es6/9_intro_to_classes.js
@@ -176,31 +176,298 @@ function cl(text) {
 
 
 // static elements in ES5
-function PersonType(name) {
-  this.name = name;
-}
-PersonType.create = function(name) { // static method - Like Ruby class method
-  return new PersonType(name);
-}
-PersonType.prototype.sayname = function() { // example method - like Ruby instance method
-  cl(this.name);
-}
-var zbych = PersonType.create('zbych');
-zbych.sayname()
+// function PersonType(name) {
+//   this.name = name;
+// }
+// PersonType.create = function(name) { // static method - Like Ruby class method
+//   return new PersonType(name);
+// }
+// PersonType.prototype.sayname = function() { // example method - like Ruby instance method
+//   cl(this.name);
+// }
+// var zbych = PersonType.create('zbych');
+// zbych.sayname()
 
 
 
-// static elements in ES6
-class PersonClass {
-  constructor(name) {
-    this.name = name;
-  }
-  sayName() { // like instance method in Ruby
-    cl(this.name);
-  }
-  static create(name) { // like class method in Ruby
-    return new PersonClass(name);
+// // static elements in ES6
+// class PersonClass {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   sayName() { // like instance method in Ruby
+//     cl(this.name);
+//   }
+//   static create(name) { // like class method in Ruby
+//     return new PersonClass(name);
+//   }
+// }
+// let stach = PersonClass.create('stach');
+// stach.sayName();
+
+
+
+// ES5 inheritance
+// function Rectangle(length, width) {
+//   this.length = length;
+//   this.width = width;
+// }
+// Rectangle.prototype.getArea = function() {
+//   return this.length * this.width;
+// }
+// function Square(length) {
+//   Rectangle.call(this, length, length);
+// }
+// Square.prototype = Object.create(Rectangle.prototype, {
+//   constructor: {
+//     // value: square,
+//     enumerable: true,
+//     writeable: true,
+//     configurable: true
+//   }
+// });
+// var square = new Square(4);
+// cl(square.getArea());
+// cl(square instanceof Square);
+// cl(square instanceof Rectangle);
+
+//ES6 inheritance
+// class Rectangle {
+//   constructor(length, width) {
+//     this.length = length
+//     this.width = width
+//   }
+//   getArea() {
+//     return this.length * this.width
+//   }
+// }
+// class Square extends Rectangle {
+//   constructor(length) {
+//     super(length, length)
+//   }
+// }
+// var square = new Square(3)
+// cl(square.getArea())
+// cl(square instanceof Square)
+// cl(square instanceof Rectangle)
+
+
+
+// using 'constructor' keyword 'super()' must be present
+// below classes are the same
+// class Square extends Rectangle {
+//   // 
+// }
+// class Square extends Rectangle {
+//   constructor(...args) {
+//     super(...args)
+//   }
+//   //
+// }
+
+
+
+// overriding methods
+// class Square extends Rectangle {
+//   constructor(length) {
+//     super(length, length)
+//   }
+//   getArea() {
+//     // return this.length * this.length
+//     return super.getArea()
+//   }
+// }
+// let square = new Square(5)
+// cl(square.getArea())
+
+
+
+// Inheriting static components
+// class Rectangle {
+//   constructor(length, width) {
+//     this.length = length
+//     this.width = width
+//   }
+//   getArea() {
+//     return this.length * this.width
+//   }
+//   static create(length, width) { // similar to class method
+//     return new Rectangle(length, width)
+//   }
+// }
+// class Square extends Rectangle {
+//   constructor(length) {
+//     super(length, length)
+//   }
+// }
+// let rect = Square.create(4, 5)
+// cl(rect instanceof Rectangle) // => true
+// cl(rect.getArea())
+// cl(rect instanceof Square) // => false !!!
+
+
+
+// inheriting after function/method
+// function Rectangle(length, width) {
+//   this.length = length
+//   this.width = width
+// }
+// Rectangle.prototype.getArea = function() {
+//   return this.length * this.width
+// }
+// function getbase() {
+//   return Rectangle
+// }
+// class Square extends getbase() {
+//   constructor(length) {
+//     super(length, length)
+//   }
+// }
+// var x = new Square(4)
+// cl(x.getArea())
+// cl(x instanceof Square)
+// cl(x instanceof Rectangle)
+
+
+
+// with dynamically inheriting after function we can do mixins (like Ruby modules)
+// let SerializableMixin = {
+//   serialize() {
+//     return JSON.stringify(this)
+//   }
+// };
+// let AreaMixin = {
+//   getArea() {
+//     return this.length * this.width
+//   }
+// }
+// function mixin(...mixins) {
+//   var base = function() {};
+//   Object.assign(base.prototype, ...mixins) // use Object's method 'assign' to assign mixin objects to base.prototype
+//   return base
+// }
+// class Square extends mixin(SerializableMixin, AreaMixin) {
+//   constructor(length) {
+//     super();
+//     this.length = length;
+//     this.width = length
+//   }
+// }
+// var x = new Square(4)
+// cl(x.getArea())
+// cl(x.serialize())
+
+
+
+// inheritinh after builin classes
+// class MyArray extends Array {
+
+// }
+// var colors = new MyArray()
+// colors[0] = 'blue'
+// cl(colors.length)
+// colors.length = 0
+// cl(colors[0])
+// let items = new MyArray(1, 2, 3, 4),
+//     subitems = items.slice(1, 3)
+// cl(items)
+// cl(items instanceof MyArray) // true
+// cl(subitems)
+// cl(subitems instanceof MyArray) // true
+
+
+
+// implementing inheriting after builtin classes
+// class MyClass {
+//   static get [Symbol.species]() {
+//     return this;
+//   }
+//   constructor(value) {
+//     this.value = value;
+//   }
+//   clone() {
+//     return new this.constructor[Symbol.species](this.value);
+//   }
+// }
+// let x = new MyClass(4)
+// let y = x.clone()
+// x.value = 5
+// cl(y.value)
+// cl(x.value)
+
+
+
+// class DerivedClass1 extends MyClass {
+//   // like Ruby class method 'def self.methodname'
+// }
+// class DerivedClass2 extends MyClass {
+//   // like Ruby class method 'def MyClass.methodname'
+//   static get [Symbol.species]() {
+//     return MyClass
+//   }
+// }
+// let x = new DerivedClass1("foo")
+// clone1 = x.clone()
+// let y = new DerivedClass2("bar")
+// clone2 = y.clone()
+
+// cl(clone1 instanceof MyClass)
+// cl(clone1 instanceof DerivedClass1)
+// cl(clone2 instanceof MyClass)
+// cl(clone2 instanceof DerivedClass2)
+
+
+
+// iverride Symbol.species to not use default class
+// class MyArray extends Array {
+//   static get [Symbol.species]() {
+//     return Array
+//   }
+// }
+// let items = new MyArray(1,2,3,4)
+// let subitems = items.slice(1,3)
+// cl(items instanceof MyArray)
+// cl(subitems instanceof Array)
+// cl(subitems instanceof MyArray)
+
+
+
+// new.target
+// class Rectangle {
+//   constructor(length, width) {
+//     cl(new.target === Rectangle) // true
+//     this.length = length
+//     this.width = width
+//   }
+// }
+// // var obj = new Rectangle(3, 4)
+
+
+
+// class Square extends Rectangle {
+//   constructor() {
+//     super(length, width)
+//   }
+// }
+// var obj = new Square(3) // false
+
+
+
+// Abstract base class
+class Shape {
+  constructor() {
+    if (new.target === Shape) {
+      throw new Error("You can not create example of abstract base class")
+    }
   }
 }
-let stach = PersonClass.create('stach');
-stach.sayName();
+class Rectangle extends Shape {
+  constructor(length, width) {
+    super()
+    this.length = length
+    this.width = width
+  }
+}
+// var x = new Shape
+var y = new Rectangle(3,4)
+cl(y instanceof Shape)
