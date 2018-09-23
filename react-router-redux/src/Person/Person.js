e152a464ffe092ec4691b055e1c5a12ada8ef82b
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-09-23 22:29:47 +0200

    Remove radium

--- react-router-redux/src/Person/Person.js
+++ react-router-redux/src/Person/Person.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import Radium from 'radium'
+// import Radium from 'radium'
 import './Person.css'
 
 const person = (props) => {
-	const style = {
-		'@media (min-width: 500px)': {
-			width: '450px'
-		}
-	}
   return (
     <div className="Person" style={style}>
       <p onClick={props.click}>yello, {props.name} here. I have {props.age} years old</p>
@@ -17,4 +12,4 @@ const person = (props) => {
   )
 }
 
-export default Radium(person)
+export default person
