3911bbb2cee7f5ce4c2cdc4ce1237897fb86f688
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-10-01 21:44:51 +0200

    Add more ingredients button handling

--- react-router-redux/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ react-router-redux/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    sheese: 0.6,
+    meat: 0.4,
+    bacon: 0.7
+
+}
 
 class BurgerBuilder extends Component {
     state = {
@@ -10,16 +19,35 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updatedCount = oldCount + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredienthandler = (type) => {
+
+    }
+    
 
     render() { 
         return (
             <Aux>
-                <div>
-                    <Burger ingredients={this.state.ingredients}/>
-                </div>
-                <div>BuildControls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                />
             </Aux>
         );
     }
