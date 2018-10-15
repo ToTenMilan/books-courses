9188400c9c4be87c64af87b19bbd856932bd754a
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-10-15 21:41:37 +0200

    Intro to Gatsby

---
+++ gatsby/hello-world/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+import Typography from "typography"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText=".... dang" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
