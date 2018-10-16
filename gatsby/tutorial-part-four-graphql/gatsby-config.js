2f0b786aeabae3320681631c4982c5b118ba2ac6
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-10-16 18:36:49 +0200

    Add graphql querying to demosite

---
+++ gatsby/tutorial-part-four-graphql/gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	siteMetadata: {
+		title: ` I am site Data, change me in gatsby-config.js file.`,
+		yolo: `wassup`,
+	},
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "minimal-ui",
+        icon: "src/images/icon.png",
+      },
+    },
+    'gatsby-plugin-offline',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
---
+++ gatsby/tutorial-part-four-graphql/src/pages/super-funny-kitten.md
@@ -0,0 +1,8 @@
+---
+title: "Funny kitten funny fun"
+date: "2018-02-20"
+---
+
+Super funny kitten that fun the fun
+
+<iframe width="560" height="315" src="https://www.youtube.com/watch?v=vxHER3a7uDg" frameborder="0" allow-ful-screen></iframe>
