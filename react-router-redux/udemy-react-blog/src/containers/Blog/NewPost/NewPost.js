cf697848690c0e2c235904a42f8f5e579e7dac45
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-10-25 21:42:29 +0200

    use history props to redirect

--- react-router-redux/udemy-react-blog/src/containers/Blog/NewPost/NewPost.js
+++ react-router-redux/udemy-react-blog/src/containers/Blog/NewPost/NewPost.js
@@ -25,7 +25,8 @@ class NewPost extends Component {
         axios.post('/posts', post)
             .then(response => {
                 console.log(response)
-                this.setState({ submitted: true });
+                this.props.history.replace('/posts')
+                // this.setState({ submitted: true });
             })
     }
 
