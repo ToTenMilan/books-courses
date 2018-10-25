af58fef1057ee3398c6decd951f2e23cb9e3ca8c
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-10-25 21:50:31 +0200

    Add auth prop to blog and render newpost conditionally

--- react-router-redux/udemy-react-blog/src/containers/Blog/Blog.js
+++ react-router-redux/udemy-react-blog/src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import axios from '../../axios'
 
 // import Post from '../../components/Post/Post';
-import { Route, NavLink, Switch } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import Posts from '../Blog/Posts/Posts'
 import NewPost from '../Blog/NewPost/NewPost'
 // import FullPost from '../Blog/FullPost/FullPost'
@@ -10,6 +10,10 @@ import './Blog.css';
 // import axios from 'axios'
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render () {
         return (
             <div className="Blog">
@@ -38,8 +42,9 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
                 {/* Switch lets match routes one at a time */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
                     <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
                 </Switch>
                 
             </div>
