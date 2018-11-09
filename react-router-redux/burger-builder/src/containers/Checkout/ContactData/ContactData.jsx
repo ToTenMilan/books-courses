792fc32347c4ce13098763ca19bd8efc2d2f4f88
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-11-09 23:37:04 +0100

    Create a custom dunamic input component

--- react-router-redux/burger-builder/src/containers/Checkout/ContactData/ContactData.jsx
+++ react-router-redux/burger-builder/src/containers/Checkout/ContactData/ContactData.jsx
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css'
 import axios from '../../../axios-orders'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
@@ -50,10 +51,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="your name" />
-                <input className={classes.Input} type="email" name="email" placeholder="your mail" />
-                <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
+                <Input inputtype="input" type="text" name="name" placeholder="your name" />
+                <Input inputtype="input" type="email" name="email" placeholder="your mail" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal code" />
                 <Button btnType="Success" clicked={this.orderHandler}>Order now</Button>
             </form>
         )
