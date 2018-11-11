54237b1ea3f95908bf39fad4c2a33519da9c0ce8
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-11-11 12:21:13 +0100

    Handling user input

--- react-router-redux/burger-builder/src/components/UI/Input/Input.jsx
+++ react-router-redux/burger-builder/src/components/UI/Input/Input.jsx
@@ -7,18 +7,19 @@ const input = (props) => {
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
             break;
         case ('select'):
             inputElement = (
                 <select
                     className={classes.InputElement}
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed} >
                     {props.elementConfig.options.map(option => (
-                        <option key={option.value} value={option.value}>
+                        <option key={option.value} value={option.value} >
                             {option.displayValue}
                         </option>
                     ))}
@@ -26,7 +27,7 @@ const input = (props) => {
             )
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
     }
 
     return (
