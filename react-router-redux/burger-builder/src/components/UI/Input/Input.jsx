a9cf6e808d77815fcd7f5709156eda11ae75d29c
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-11-11 13:43:23 +0100

    Adding validation feedback

--- react-router-redux/burger-builder/src/components/UI/Input/Input.jsx
+++ react-router-redux/burger-builder/src/components/UI/Input/Input.jsx
@@ -4,18 +4,23 @@ import classes from './Input.css'
 
 const input = (props) => {
     let inputElement = null
+    const inputClasses = [classes.InputElement]
+
+    if (props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid)
+    }
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
+            inputElement = <textarea className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />
             break;
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     value={props.value}
                     onChange={props.changed} >
                     {props.elementConfig.options.map(option => (
@@ -27,7 +32,7 @@ const input = (props) => {
             )
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed} />
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed} />
     }
 
     return (
--- react-router-redux/burger-builder/src/containers/Checkout/ContactData/ContactData.jsx
+++ react-router-redux/burger-builder/src/containers/Checkout/ContactData/ContactData.jsx
@@ -118,7 +118,7 @@ class ContactData extends Component {
     }
 
     checkValidity(value, rules) {
-        let isValid = false
+        let isValid = true
         if (rules.required) {
             isValid = value.trim() !== '' && isValid;
         }
@@ -162,6 +162,8 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} /> // () => this.inChHa() to evaluate this method only for specific input instead globally
                 ))}
                 <Button btnType="Success">Order now</Button>
