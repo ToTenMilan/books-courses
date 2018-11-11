98b7227535989dfde5489e64f389e686ad7f7e2b
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-11-11 11:25:28 +0100

    Adding dropdown component

--- react-router-redux/burger-builder/src/components/UI/Input/Input.jsx
+++ react-router-redux/burger-builder/src/components/UI/Input/Input.jsx
@@ -12,6 +12,19 @@ const input = (props) => {
         case ('textarea'):
             inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />
             break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            )
+            break;
         default:
             inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
     }
