36702ec30a2907266016d2ca63d25da18abe1a48
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-11-17 11:41:58 +0100

    Menage purchasable in local state

--- react-router-redux/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ react-router-redux/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions'
 
 class BurgerBuilder extends Component {
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -38,7 +37,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el
             }, 0)
-        this.setState({purchasable: sum > 0})
+        return sum > 0
     }
 
     purchaseHandler = () => {
@@ -79,7 +78,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
                         ordered={this.purchaseHandler}
                     />
