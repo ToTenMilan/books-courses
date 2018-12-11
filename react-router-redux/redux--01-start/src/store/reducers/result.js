7d66c32e65ba383d3c4f1a6b72cbbf0e223201dc
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-11 21:43:24 +0100

    Leaner switch case statement

--- react-router-redux/redux--01-start/src/store/reducers/result.js
+++ react-router-redux/redux--01-start/src/store/reducers/result.js
@@ -6,16 +6,17 @@ const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElementId); // returns new array
+    return updateObject(state, {results: updatedArray})
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             return updateObject(state, { results: state.results.concat({id: new Date(), value: action.result })})
         case actionTypes.DELETE_RESULT:
-            // const id = 2
-            // const newAray = [...state.results] // create a copy
-            // newAray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElementId); // returns new array
-            return updateObject(state, {results: updatedArray})
+            return deleteResult(state, action)
 
     }
     return state
