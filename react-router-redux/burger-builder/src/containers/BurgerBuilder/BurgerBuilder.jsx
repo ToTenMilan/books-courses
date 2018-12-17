863c7c36d67d0ee672e2ecce241e6fcd2541af7b
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-17 21:23:50 +0100

    Initialize ingredients in the burgerbuilder

--- react-router-redux/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ react-router-redux/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -18,7 +18,7 @@ class BurgerBuilder extends Component {
 
     componentDidMount () {
         console.log(this.props)
-
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState (ingredients) {
@@ -52,7 +52,7 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
         let orderSummary = null
-        let burger = this.state.error ? <p>Ingredients can't be loaded. Try again in few seconds</p> : <Spinner />
+        let burger = this.props.error ? <p>Ingredients can't be loaded. Try again in few seconds</p> : <Spinner />
         if (this.props.ings) {
             burger = (
                 <Aux>
@@ -95,14 +95,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
     }
 }
  
--- react-router-redux/burger-builder/src/store/actions/burgerBuilder.js
+++ react-router-redux/burger-builder/src/store/actions/burgerBuilder.js
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-            axios.get('https://react-burger-3.firebaseio.com/ingredients.json')
+        axios.get('https://react-burger-3.firebaseio.com/ingredients.json')
             .then(response => {
                 dispatch(setIngredients(response.data))
             })
