1ac03cf0754603f651fe152173fe99c2bf5626a9
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-22 21:19:25 +0100

    Add Redirect to root from empty checkout

--- react-router-redux/burger-builder/src/containers/Checkout/Checkout.jsx
+++ react-router-redux/burger-builder/src/containers/Checkout/Checkout.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
@@ -13,18 +13,23 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data')
     }
 
-    render() { 
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />
-            </div>
-        );
+    render() {
+        let summary = <Redirect to="/" />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler} />
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData} />
+                </div>
+                
+            )
+        }
+        return summary;
     }
 }
 
