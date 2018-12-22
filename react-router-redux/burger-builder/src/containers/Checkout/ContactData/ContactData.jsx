e3285a85432733da1e9c72ec44cd3dfaff65267b
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-22 21:36:13 +0100

    Combining reducers

--- react-router-redux/burger-builder/src/containers/Checkout/ContactData/ContactData.jsx
+++ react-router-redux/burger-builder/src/containers/Checkout/ContactData/ContactData.jsx
@@ -199,9 +199,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingrediens,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 }
 
