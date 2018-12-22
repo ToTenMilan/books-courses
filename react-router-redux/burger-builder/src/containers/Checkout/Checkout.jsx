db4768b2983effaa33099231d49c570d6aa44fad
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-23 00:27:44 +0100

    Add redirect to root after purchase

--- react-router-redux/burger-builder/src/containers/Checkout/Checkout.jsx
+++ react-router-redux/burger-builder/src/containers/Checkout/Checkout.jsx
@@ -3,6 +3,7 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 import ContactData from './ContactData/ContactData'
+import * as actions from '../../store/actions'
 
 class Checkout extends Component {
     checkoutCancelledHandler = () => {
@@ -15,9 +16,12 @@ class Checkout extends Component {
 
     render() {
         let summary = <Redirect to="/" />
+        
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to ="/" /> : null
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -36,6 +40,7 @@ class Checkout extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 }
  
--- react-router-redux/burger-builder/src/store/reducers/order.js
+++ react-router-redux/burger-builder/src/store/reducers/order.js
@@ -2,7 +2,8 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     orders: [],
-    loading: false
+    loading: false,
+    purchased: false
 }
 
 const reducer = (state = initialState, action) => {
@@ -20,6 +21,7 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 loading: false,
+                purchased: true,
                 orders: state.orders.concat(newOrder)
             };
         case actionTypes.PURCHASE_BURGER_FAIL:
@@ -27,6 +29,11 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 loading: false
             };
+        case actionTypes.PURCHASE_INIT:
+            return {
+                ...state,
+                purchased: false
+            }
         default:
             return state;
     }
