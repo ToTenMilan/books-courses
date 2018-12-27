26d8fb636d0c6a9b8cf40d26c07edafbb264ab11
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-27 20:16:05 +0100

    Get token from the backend

--- react-router-redux/burger-builder/src/store/actions/auth.js
+++ react-router-redux/burger-builder/src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,6 +25,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
-    }
-    
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDuKkDOhS-b2_XXGrsBn2Jcbrbr4a7xyzo', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
+    } 
 }
