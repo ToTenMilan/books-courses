bcf65fbf832cee6ffe48b73a2436f557150affba
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-30 11:32:32 +0100

    Add logout link

--- react-router-redux/burger-builder/src/hoc/Layout/Layout.js
+++ react-router-redux/burger-builder/src/hoc/Layout/Layout.js
@@ -4,6 +4,7 @@ import Aux from '../Aux/Aux'
 import classes from './Layout.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+import { connect } from 'react-redux';
 
 class Layout extends Component {
 	state = {
@@ -29,8 +30,13 @@ class Layout extends Component {
 	render() {
 		return (
 			<Aux>
-				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-				<SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+				<Toolbar
+					isAuth={this.props.isAuthenticated}
+					drawerToggleClicked={this.sideDrawerToggleHandler} />
+				<SideDrawer
+					isAuth={this.props.isAuthenticated}
+					open={this.state.showSideDrawer}
+					closed={this.sideDrawerClosedHandler}/>
 				<main className={classes.Content}>
 					{this.props.children}
 				</main>
@@ -39,4 +45,10 @@ class Layout extends Component {
 	}
 }
 
-export default Layout
+const mapStateToProps = state => {
+	return {
+		isAuthenticated: state.auth.token !== null
+	}
+}
+
+export default connect(mapStateToProps)( Layout )
