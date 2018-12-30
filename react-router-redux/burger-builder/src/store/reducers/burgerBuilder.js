80d8c909b3508f260ec3365608eea50ea45f72e2
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2018-12-30 17:13:50 +0100

    Redirect the user to the checkout page

--- react-router-redux/burger-builder/src/store/reducers/burgerBuilder.js
+++ react-router-redux/burger-builder/src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from '../utility';
 const initialState = {
     ingredients: null,
     totalPrice: 4,
-    error: false
+    error: false,
+    building: false
 }
 
 const INGREDIENT_PRICES = {
@@ -19,7 +20,8 @@ const addIngredient = ( state, action ) => {
     const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
     const updatedState = {
         ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
     }
     return updatedState
 }
@@ -38,7 +40,8 @@ const setIngredients = (state, action) => {
     return updateObject( state, {
         ingredients: action.ingredients,
         totalPrice: 4,
-        error: false
+        error: false,
+        building: false
     })
 }
 
