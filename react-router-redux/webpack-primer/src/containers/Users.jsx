15cc2ede71cba006a1b1d56d320c6bff83881cc4
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-01-01 21:15:42 +0100

    Create basic react application

---
+++ react-router-redux/webpack-primer/src/containers/Users.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    render() { 
+        return (
+            <div>
+                <h1>The Pizza</h1>
+                <p>users of the course</p>
+            </div>
+        );
+    }
+}
+ 
+export default Users;
