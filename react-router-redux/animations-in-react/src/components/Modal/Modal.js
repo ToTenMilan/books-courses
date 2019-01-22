c6bdfe4d62addd557d6220da40ade8293cb95ea8
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-01-22 21:25:49 +0100

    Animations and transitions limitations

--- react-router-redux/animations-in-react/src/components/Modal/Modal.js
+++ react-router-redux/animations-in-react/src/components/Modal/Modal.js
@@ -3,7 +3,10 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-    const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+    const cssClasses = [
+        'Modal',
+        props.show ? 'ModalOpen' : 'ModalClosed'
+    ];
 
     return (
         <div className={cssClasses.join(' ')}>
