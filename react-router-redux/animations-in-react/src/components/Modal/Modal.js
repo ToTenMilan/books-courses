992556ac47b8fd4d2b0d582074fb06e28ccd5770
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-01-23 21:33:41 +0100

    Transition events

--- react-router-redux/animations-in-react/src/components/Modal/Modal.js
+++ react-router-redux/animations-in-react/src/components/Modal/Modal.js
@@ -3,13 +3,18 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 400,
+    exit: 1000,
+}
+
 const modal = (props) => {
     return (
         <Transition
             mountOnEnter
             unmountOnExit
             in={props.show}
-            timeout={300}>
+            timeout={animationTiming}>
             {state => {
                 const cssClasses = [
                     'Modal',
