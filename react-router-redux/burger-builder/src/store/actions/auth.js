fd9d297197dcc98928d2dc84c65ea23f7e2faab3
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-01-31 21:33:34 +0100

    Hook up logout saga

--- react-router-redux/burger-builder/src/store/actions/auth.js
+++ react-router-redux/burger-builder/src/store/actions/auth.js
@@ -79,7 +79,7 @@ export const authCheckState = () => {
     return dispatch => {
         const token = localStorage.getItem('token');
         if (!token) {
-            dispatch(logout)
+            dispatch(logout())
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
             if (expirationDate <= new Date()) {
