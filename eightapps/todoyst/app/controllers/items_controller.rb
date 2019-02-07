f69c5d0f00d9ab3d79352d06d5707f85996fbf7e
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-07 21:20:24 +0100

    Index and show actions

--- eightapps/todoyst/app/controllers/items_controller.rb
+++ eightapps/todoyst/app/controllers/items_controller.rb
@@ -1,2 +1,35 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.all
+  end
+  
+  def new
+    @item = Item.new
+  end
+
+  def show
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:success] = "Item successfully created"
+      redirect_to root_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
