814ee05dd040208767c85c15e9c13d48f371ef78
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-10 21:02:39 +0100

    Add edit update delete messages actions

--- eightapps/espn/app/controllers/messages_controller.rb
+++ eightapps/espn/app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
   def index
     @messages = Message.all.order('created_at DESC')
   end
@@ -8,9 +9,7 @@ class MessagesController < ApplicationController
   end
 
   def show
-    @message = Message.find(params[:id])
   end
-  
   
   def create
     @message = Message.new(message_params)
@@ -23,9 +22,37 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @message.update_attributes(params[:message])
+      flash[:success] = "Message was successfully updated"
+      redirect_to @message
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @message.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to messages_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to messages_url
+    end
+  end
+  
+
   private
 
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
