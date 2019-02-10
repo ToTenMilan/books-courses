98ecbedaee0300b1c5ea3983dfb94e8247353999
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-10 19:40:03 +0100

    Add view and create action for messages

--- eightapps/espn/app/controllers/messages_controller.rb
+++ eightapps/espn/app/controllers/messages_controller.rb
@@ -3,4 +3,24 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
   
+  def new
+    @message = Message.new
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success] = "Message successfully created"
+      redirect_to @message
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :description)
+  end
 end
