4e457950458f6ef5fceeed2f6f507faf216ffe77
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-10 21:35:05 +0100

    Relate users and messages

--- eightapps/espn/app/controllers/messages_controller.rb
+++ eightapps/espn/app/controllers/messages_controller.rb
@@ -1,18 +1,18 @@
 class MessagesController < ApplicationController
   before_action :find_message, only: [:show, :edit, :update, :destroy]
   def index
-    @messages = Message.all.order('created_at DESC')
+    @messages = current_user.messages.order('created_at DESC')
   end
   
   def new
-    @message = Message.new
+    @message = current_user.messages.new
   end
 
   def show
   end
   
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
     if @message.save
       flash[:success] = "Message successfully created"
       redirect_to @message
