57a6dc259463e547e7ff09bd26a9f21fbfb491b5
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-10 20:15:23 +0100

    Add index and show

--- eightapps/espn/app/controllers/messages_controller.rb
+++ eightapps/espn/app/controllers/messages_controller.rb
@@ -1,11 +1,16 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.all.order('created_at DESC')
   end
   
   def new
     @message = Message.new
   end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+  
   
   def create
     @message = Message.new(message_params)
