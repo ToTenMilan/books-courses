312ed2dfbee133b10eb0c93c6f2d9679a5eaaa57
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-12 22:03:32 +0100

    Add new and create actions

--- eightapps/boredway/app/controllers/plays_controller.rb
+++ eightapps/boredway/app/controllers/plays_controller.rb
@@ -2,5 +2,26 @@ class PlaysController < ApplicationController
   def index
     @plays = Play.all
   end
+
+  def new
+    @play = Play.new
+  end
+  
+  def create
+    @play = Play.new(play_params)
+    if @play.save
+      flash[:success] = "Play successfully created"
+      redirect_to @play
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end
   
 end
