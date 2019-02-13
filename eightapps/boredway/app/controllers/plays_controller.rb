47715dd32583fc540355437c159fa37a2eea4d7d
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-13 22:05:35 +0100

    Add Plays to app index and show actions

--- eightapps/boredway/app/controllers/plays_controller.rb
+++ eightapps/boredway/app/controllers/plays_controller.rb
@@ -1,17 +1,21 @@
 class PlaysController < ApplicationController
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
   def index
-    @plays = Play.all
+    @plays = Play.all.order('created_at DESC')
   end
 
   def new
     @play = Play.new
   end
-  
+
+  def show
+  end
+
   def create
     @play = Play.new(play_params)
     if @play.save
       flash[:success] = "Play successfully created"
-      redirect_to @play
+      redirect_to root_path
     else
       flash[:error] = "Something went wrong"
       render 'new'
@@ -23,5 +27,8 @@ class PlaysController < ApplicationController
   def play_params
     params.require(:play).permit(:title, :description, :director)
   end
-  
+
+  def find_play
+    @play = Play.find(params[:id])
+  end  
 end
