ab566867f6a3676041c2b0e2167654bad14cd5ec
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-13 22:14:08 +0100

    Add edit and delete actions

--- eightapps/boredway/app/controllers/plays_controller.rb
+++ eightapps/boredway/app/controllers/plays_controller.rb
@@ -21,7 +21,30 @@ class PlaysController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end
   
+  def update
+    if @play.update_attributes(play_params)
+      flash[:success] = "Object was successfully updated"
+      redirect_to @play
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    if @play.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to plays_url
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to plays_url
+    end
+  end
+
   private
 
   def play_params
