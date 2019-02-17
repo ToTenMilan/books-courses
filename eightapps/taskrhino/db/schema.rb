bd3fe8e5ca26d0110235001f924bbc9008879e63
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-17 20:47:59 +0100

    Add categories to tasks

--- eightapps/taskrhino/db/schema.rb
+++ eightapps/taskrhino/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_182142) do
+ActiveRecord::Schema.define(version: 2019_02_17_191437) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_17_182142) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
