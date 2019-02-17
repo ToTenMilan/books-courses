70c2af5fdb210e0e62b348629d716958c0a70289
Milan <grzegorz.od.tych.milanowskich@gmail.com>
2019-02-17 14:36:22 +0100

    Create Reviews

---
+++ eightapps/boredway/app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def def new
+    find_play
+    @review = Review.new
+  end
+
+  def create
+    find_play
+    @review = Review.new(review_params)
+    @review.update(play_id: @play.id, user_id: current_user.id)
+    if @review.save
+      flash[:success] = "Object successfully created"
+      redirect_to play_path(@play)
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+
+    def find_play
+      @play = Play.find(params[:play_id])
+    end
+end
--- eightapps/boredway/db/schema.rb
+++ eightapps/boredway/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_120343) do
+ActiveRecord::Schema.define(version: 2019_02_17_131601) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 2019_02_17_120343) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "play_id"
   end
 
   create_table "users", force: :cascade do |t|
