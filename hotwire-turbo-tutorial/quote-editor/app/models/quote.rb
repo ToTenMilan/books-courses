f390fa19537aa3d3c164dbc35759be2d4aed343b
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2023-05-23 22:19:43 +0200

    broadcast created quote through websocket

--- hotwire-turbo-tutorial/quote-editor/app/models/quote.rb
+++ hotwire-turbo-tutorial/quote-editor/app/models/quote.rb
@@ -1,5 +1,12 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
 
+  after_create_commit -> {
+    broadcast_prepend_to 'quotes', # should be broadcasted to users subscribed to the "quotes" stream (ensure)
+    partial: 'quotes/quote',
+    locals: { quote: self },
+    target: 'quotes'#, prepended to the DOM node with the id of "quotes" (ensure)
+  }
+
   scope :ordered, -> { order(id: :desc) }
 end
