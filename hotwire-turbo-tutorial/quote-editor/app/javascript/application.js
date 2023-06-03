1ccad48652230b2a62dcc17cf05b201ca1f1084d
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2023-06-03 18:20:56 +0200

    add flash messages and turn off turbo

--- hotwire-turbo-tutorial/quote-editor/app/javascript/application.js
+++ hotwire-turbo-tutorial/quote-editor/app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+//import "@hotwired/turbo-rails"
 import "./controllers"
+//import "@hotwired/turbo-rails"
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
