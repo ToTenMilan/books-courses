ab789614e3c6f4aa8e2aa6935c5d54c7167e7125
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2023-06-10 17:51:38 +0200

    setup LineItemDate controller

--- hotwire-turbo-tutorial/quote-editor/app/helpers/application_helper.rb
+++ hotwire-turbo-tutorial/quote-editor/app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def render_turbo_stream_flash_message
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
+
+  def form_error_notification(object)
+    if object.errors.any?
+      tag.div class: "error-message" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
 end
