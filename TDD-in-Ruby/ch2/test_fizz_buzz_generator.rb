aca724a4895f0d78e11116bb5cdbe0b8d7a9c8a5
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-01-15 16:10:31 +0100

    Make fizzbuz open-closed principle

---
+++ TDD-in-Ruby/ch2/test_fizz_buzz_generator.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require_relative 'fizz_buzz_generator'
+
+class TestFizzBuzzGenerator < Minitest::Test
+  def  test_fizz_buzz_sequence
+    # Open-Close principle example
+    objects = [FizzBuzz.new, Fizz.new, Buzz.new, NoFizzBuzz.new]
+    g = FizzBuzzGenerator.new(objects, (1..20).to_a)
+
+    result = g.generate
+    expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz"]
+
+    assert_equal expected, result
+  end
+end
