28ca591aa380b45a12c74bca9495736e29d45aab
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-02-06 12:26:10 +0100

    follow triangulation process

--- TDD-in-Ruby/ch3/calculator/test_addition.rb
+++ TDD-in-Ruby/ch3/calculator/test_addition.rb
@@ -2,11 +2,23 @@ require 'minitest/autorun'
 
 class Calculator
   def add(addend, augend)
-    addend+augend
+    # "fake it till you kmake it" chapter
+    # addend+augend
+
+    # triangulation chapter
+    addend + augend
   end
 end
 
 class TestCaculator < Minitest::Test
+  # "fake it till you kmake it" chapter
+  # def test_addition
+  #   calculator = Calculator.new
+  #   result = calculator.add(1, 2)
+  #   assert_equal 3, result
+  # end
+
+  # triangulation chapter
   def test_addition
     calculator = Calculator.new
     result = calculator.add(1, 2)
