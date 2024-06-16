8d7e3697ff7b261c38abe2852329580007270e29
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-06-16 19:11:55 +0200

    Configure sidekiq

--- sidekiq-book/config/routes.rb
+++ sidekiq-book/config/routes.rb
@@ -1,8 +1,8 @@
-# require 'sidekiq/web'
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :orders, only: [ :new, :create, :show ]
   resources :simulated_behaviors, only: [ :edit, :update ]
   root "welcome#show"
-  # mount Sidekiq::Web => "/sidekiq"
+  mount Sidekiq::Web => "/sidekiq"
 end
