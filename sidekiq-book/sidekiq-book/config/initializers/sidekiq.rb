2b065c7658f57b314cd96ecf63393182514588e7
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-06-17 11:35:23 +0200

    Notify for failed jobs after they stop retrying

--- sidekiq-book/sidekiq-book/config/initializers/sidekiq.rb
+++ sidekiq-book/sidekiq-book/config/initializers/sidekiq.rb
@@ -1,5 +1,12 @@
 Sidekiq.configure_server do |config|
+  config.death_handlers << ->(job, exception) {
+    ErrorCatcherServiceWrapper.new.notify(
+      "#{job['class']} won't be retried: #{exception.message}"
+    )
+  }
+
   config.error_handlers << ->(exception, context_hash) {
     ErrorCatcherServiceWrapper.new.notify(exception)
   }
+
 end
